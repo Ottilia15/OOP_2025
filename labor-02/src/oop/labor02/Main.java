db562e52005575711235a691899e90366636690c
szekely.ottilia <szekely.ottilia@student.ms.sapientia.ro>
2025-02-26 09:38:35 +0200

    Labor 2 classwork

--- labor-02/src/oop/labor02/Main.java
+++ labor-02/src/oop/labor02/Main.java
@@ -1,7 +1,15 @@
 package oop.labor02;
 
+import oop.labor02.bank.BankAccount;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Lab_02");
+     BankAccount account1 = new BankAccount("BT68264818");
+        System.out.println(account1.getAccountNumber());
+        System.out.println(account1.getBalance());
+        account1.deposit(100);
+        account1.deposit(-200);
+        System.out.println("New balance: ");
+        System.out.println(account1.getBalance());
     }
 }
